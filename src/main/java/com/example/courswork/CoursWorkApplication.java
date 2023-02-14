5b19d0287a026f6804726eec3f4adcc2f460d59f
mdrag <m.dragay@yandex.ru>
2023-02-14 14:35:22 +0300

    Initial commit

---
+++ src/main/java/com/example/courswork/CoursWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.courswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courswork/CoursWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
